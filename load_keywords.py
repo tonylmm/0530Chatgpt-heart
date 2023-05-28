c0bc01f906b7b93038f0ffc9c40a0abfa0786b31
tonylmm <123880923+tonylmm@users.noreply.github.com>
2023-05-28 16:23:00 +0800

    Update load_keywords.py

--- load_keywords.py
+++ load_keywords.py
@@ -2,7 +2,7 @@ import pandas as pd
 
 def load_keywords():
     # 讀取csv檔案
-    df = pd.read_csv('EAP相關網站.csv')
+    df = pd.read_csv('1 EAP websites.csv')
 
     # 將csv檔案轉換為字典
     keywords_dict = df.set_index('關鍵字')['回應內容'].to_dict()
